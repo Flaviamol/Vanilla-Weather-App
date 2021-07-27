d73657a31a076dddd54b4e9ef3f96f93f94ee452
Flaviamol <flaviacm.de@gmail.com>
2021-07-27 09:17:22 +0200

    two different ways of doing the hour and day update

--- src/app.js
+++ src/app.js
@@ -25,18 +25,37 @@ if (minutes < 10) {
 let currentDay = document.querySelector(".default-subtitle");
 currentDay.innerHTML = `${day} ${hours}:${minutes}`;
 
+// function formatDate(timeStamp) {
+//   let date = new Date(timeStamp);
+//   let hours = date.getHours();
+//   let minutes = date.getMinutes();
+//   let days = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ];
+//   let day = days[now.getDay()];
+//   return `${day} ${hours}:${minutes}`;
+// }
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector(".current-temperature");
   let cityElement = document.querySelector(".default-city-name");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  //   let dateElement = document.querySelector(".default-subtitle");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   console.log(response.data);
   humidityElement.innerHTML = `${response.data.main.humidity}%`;
   windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+  //   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${apiKey}&units=${units}`;
